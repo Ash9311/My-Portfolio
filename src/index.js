315602e397b30d58cc0f8090416f955a724cd2ab
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-10-31 00:48:08 +0530

    setting up -2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import ReactDom from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />,document.getElementById('root');)
+reactDom.render(<App />,document.getElementById('root'));
