fb57ab2f0d1a49c8a2ea5a43e1a3dd58f6d1660e
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-10-31 15:55:13 +0530

    Components Setup 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter,Route } from 'react-router-dom';
-
+import Projects from '../Projects/Projects';
+import Home from '../Home/Home';
+import About from '../About/About';
+import Skills from '../Skills/Skills';
+import Contact from '../Contact/Contact';
 
 
 const App = () => {
     return(<div>
        <BrowserRouter>
        <div>
-           <Route path="/" component={} />
-           <Route path="/about" component={} />
-           <Route path="/projects" component={} />
-           <Route path="/skills" component={} />
-           <Route path="/contact" component={} />
+           <Route path="/" component={Home} />
+           <Route path="/about" component={About} />
+           <Route path="/projects" component={Projects} />
+           <Route path="/skills" component={Skills} />
+           <Route path="/contact" component={Contact} />
        </div>
        </BrowserRouter>
     </div>);
