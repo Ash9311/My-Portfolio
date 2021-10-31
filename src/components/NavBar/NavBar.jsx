86eb1de00675776448dcd3a5d8dc144703bf7acc
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-10-31 17:20:41 +0530

    navBar styles-1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import './NavBar.css';
 import {slide as Menu} from 'react-burger-menu';
+import {Link} from 'react-router-dom';
 
 class NavBar extends React.Component {
     state = {}
     render() {
         return ( 
             <Menu>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/projects">Projects</a>
+                {/* we never want to use <a> tag inside react router application */}
+                {/* <a id="home" className="menu-item" href="/">Home</a> */}
+                <Link to="/" className="menu-item active-item">Home</Link>
+                <Link to="/about" className="menu-item">About</Link>
+                <Link to="/projects" className="menu-item">Projects</Link>
+                <Link to="/skills" className="menu-item">Skills</Link>
+                <Link to="/contact" className="menu-item">Contact</Link>
                
             </Menu>
         );
