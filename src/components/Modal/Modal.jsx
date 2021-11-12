904aa6d0ffe88e99138f2e7597542a3c4f414689
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-11-12 09:33:29 +0530

    Modal styling - 1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,22 @@ import './Modal.css';
 const Modal = (props) => {
     return ReactDOM.createPortal(
         <div className='modal-overlay'>
-            <h1>I am the modal!</h1>
+            <div className="upper-section">
+                <h1>Huge Announcement</h1>
+                <p className="close">&times;</p>
+            </div>
+            <div className="middle-section">
+                <h3>React Mega Tutorial is live</h3>
+                <p>do you wish to learn React?</p>
+                <p>
+                    Contact me if you want to know
+                </p>
+               
+                
+            </div>
+            <div className="lower-section">
+            <a className="modal-btn btn-red">Close</a>
+            </div>
         </div>,
         document.getElementById('modal')
     );
