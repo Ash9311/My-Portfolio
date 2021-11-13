58e12c5d62783494b4bedf3f98601e2efb72ba01
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-11-13 20:38:45 +0530

    Footer Link - 1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from '../Header/Header';
 import './About.css'
+import aboutVector from './../../assets/about_vector.png';
+import FooterLink from '../FooterLink/FooterLink';
 
 const About= () => {
     return ( <div className='section-container'>
         <Header heading='About Me.'
         details='Software Engineer | B.Tech Graduate | Coding and Problem Solving Geek'
         />
+    <FooterLink phrase="check out my " link="projects!" toAdress="/projects"/>
+    {/* vector Frame */}
+    <div className="vector-frame">
+        <img src={aboutVector} className="about-vector" alt="about"/>
+    </div>
+
     </div> );
 };
 
