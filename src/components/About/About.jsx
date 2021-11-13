b4cfaf4f1b722d3852e693b44686a5b96b04e4f9
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-11-13 16:52:32 +0530

    Header - 1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from '../Header/Header';
 import './About.css'
 
 const About= () => {
-    return ( <div>
-        <h1>I am the about page!</h1>
+    return ( <div className='section-container'>
+        <Header heading='About Me.'
+        details='Software Engineer | B.Tech Graduate | Coding and Problem Solving Geek'
+        />
     </div> );
-}
+};
 
 export default About;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,7 +3,7 @@ import './Skills.css';
 
 const Skills = () => {
     return(
-        <div>
+        <div  className='section-container'>
             <h1>this is skills page</h1>
         </div>
     )
