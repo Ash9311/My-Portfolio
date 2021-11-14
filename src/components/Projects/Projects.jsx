e307298b38669f26255b8e6b78a0738452b96a54
Ashutosh Mulky <ashutoshmulki7@gmail.com>
2021-11-14 17:12:55 +0530

    project - 1

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import FooterLink from "../FooterLink/FooterLink";
 import Header from "../Header/Header";
-import './Projects.css'
+import './Projects.css';
+import { projectsData } from "../../assets/projectsData";
 
 const Projects = () => {
     return(
         <div className='section-container'>
             <Header heading="My Projects." details="Here are a few cool things I've worked on"/>
+
+            <div className="project-cards-container">
+                {
+                    projectsData.map(project => {
+                        return <h1>{project.projectName}</h1>
+                    })
+                }
+            </div>
+
 
             <FooterLink phrase="Check out " link="my skills!" toAdress="/skills" />
         </div>
